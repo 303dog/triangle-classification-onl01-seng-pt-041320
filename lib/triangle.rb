58d6563503168a5fa5883c49b8295e96041e287d
Paul_Beckwith <303dog@gmail.com>
2020-05-17 12:07:12 -0600

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,5 +1,5 @@
 class Triangle
-  # s1, s2, s3 are lengths of the 3 sides
+
   attr_accessor :s1, :s2, :s3
 
   def initialize(s1, s2, s3)
@@ -7,24 +7,15 @@ class Triangle
     @s2 = s2
     @s3 = s3
   end
-# kind method
-#  compute 2 validity tests
-#  if all side lengths > 0, validity test 1 = true
-#  then yield to method to test triangle inequality spec
-#  sum of 2 sides > 3rd side; if true, continue with #kind method
-#  output kind of triangle
 
   def kind
-      # first test for nonzero & nonnegative side lengths
-      # and test for "triangle inequality" principle
-    if  (s1 <= 0 || s2 <= 0 || s3 <= 0) ||
-        (s1 >= s2 + s3 || s2 >= s1 + s3 || s3 >= s1 + s2)
+    if (s1 <= 0 || s2 <= 0 || s3 <= 0) ||
+      (s1 >= s2 + s3 || s2 >= s1 + s3 || s3 >= s1 + s2)
         begin
           raise TriangleError
-        #rescue TriangleError => error
-          puts error.message
+                puts error.message
         end
-      # if pass validity tests, determine kind of triangle
+      
     elsif s1 == s2 && s2 == s3
       :equilateral
     elsif s1 != s2 && s2 != s3 && s1 != s3
@@ -32,8 +23,7 @@ class Triangle
     elsif s1 == s2 || s2 == s3 || s1 == s3
       :isosceles
     end
-  end # kind method
-
+  end 
   class TriangleError < StandardError
     def message
       "Not a valid triangle"
